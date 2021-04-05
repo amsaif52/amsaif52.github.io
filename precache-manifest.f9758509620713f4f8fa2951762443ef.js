5c99c17af249a643f109b7f35292ca9a2ab580c3
Ali Asgar Merchant <am52saif@gmail.com>
2021-04-05 01:14:11 -0400

    Updates

---
+++ precache-manifest.f9758509620713f4f8fa2951762443ef.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c19d37cb5b60ea37d8a5ef2264d65a8",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "429dee02044d870925ed",
+    "url": "/resume/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "2404bfa19d9e99d63db9",
+    "url": "/resume/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "46c089a767bbdc770756",
+    "url": "/resume/static/css/main.bf75bbac.chunk.css"
+  },
+  {
+    "revision": "1d47de53ce5a949c9085",
+    "url": "/resume/static/js/2.19aa871a.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/resume/static/js/2.19aa871a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "429dee02044d870925ed",
+    "url": "/resume/static/js/3.97cabd14.chunk.js"
+  },
+  {
+    "revision": "2404bfa19d9e99d63db9",
+    "url": "/resume/static/js/4.aad39f38.chunk.js"
+  },
+  {
+    "revision": "46c089a767bbdc770756",
+    "url": "/resume/static/js/main.a9ed22f0.chunk.js"
+  },
+  {
+    "revision": "8220a3613a4a4acf2a69",
+    "url": "/resume/static/js/runtime-main.f22d4ef4.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/resume/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/resume/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "9a18b9624ee2cd9f6541dfb215615dba",
+    "url": "/resume/static/media/braun_logo.9a18b962.svg"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/resume/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/resume/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/resume/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/resume/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "643f7e6d27e447f8c9ef9b53a399175c",
+    "url": "/resume/static/media/joy_glee_logo.643f7e6d.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/resume/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "57528d71dd23ab3fde1ca739a0f75a1d",
+    "url": "/resume/static/media/oldspice-logo.57528d71.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/resume/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/resume/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/resume/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
